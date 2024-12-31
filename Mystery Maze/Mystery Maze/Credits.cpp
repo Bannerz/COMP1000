b380c4b7a60bebb72262bd777d8e44c2ec4af18d
Alex <alexbanfield97@gmail.com>
2024-12-31 19:59:01 +0000

    updated credits page

--- Mystery Maze/Mystery Maze/Credits.cpp
+++ Mystery Maze/Mystery Maze/Credits.cpp
@@ -13,10 +13,10 @@ void Credits::runCredits(sf::RenderWindow& window) {
 
     sf::Text creditsTitle("Credits:", font, 50);
     creditsTitle.setFillColor(sf::Color::White);
-    creditsTitle.setPosition(100, 100);
-    sf::Text creditsText("\nGame developed by Alex Banfield \nSprites: \nAlex Banfield: Heart and potions\nCazWolf: https://cazwolf.itch.io/tiny-pixel-fantasy-generator \nChris Hamons: https://opengameart.org/content/dungeon-crawl-32x32-tiles \nTutorials Used: \nMenu Tutorial: YouTube.com/watch?v=bOG8667yePY \nScreen States:YouTube.com/watch?v=Qhlqhen6cmw \nViews Documentation: https://www.sfml-dev.org/tutorials/2.6/graphics-view.php\nGame Timer, score, and Game Music: https://cplusplus.com/forum/windows/172064/\nMusic:\nMusic by Jerome Chauve: https://pixabay.com/users/abydos_music-27178115/ \n \nPress 'Enter' to return to main menu.", font, 20);
+    creditsTitle.setPosition(60, 100);
+    sf::Text creditsText("Tutorials Used:\nMenu Tutorial : YouTube.com/watch?v=bOG8667yePY\nScreen States : YouTube.com/watch?v=Qhlqhen6cmw\nViews Documentation : https://www.sfml-dev.org/tutorials/2.6/graphics-view.php\nGame Timer, score, and Game Music : https://cplusplus.com/forum/windows/172064/\nBounding box collision : https://www.youtube.com/watch?v=l2iCYCLi6MU\nHUD / UI : https ://www.youtube.com/watch?v=uqrcVITXtEM\nFont and text : https ://www.youtube.com/watch?v=8kuYOdRO894\nInput documentation : https://www.sfml-dev.org/documentation/2.6.1/classsf_1_1Keyboard.php\nTyping input : https://en.sfml-dev.org/forums/index.php?topic=26927.0 \n https://en.sfml-dev.org/forums/index.php?topic=19965.0\nSave to DAT : https://stackoverflow.com/questions/65104558/save-data-to-a-dat-file \n http://www.java2s.com/Tutorial/Cpp/0240__File-Stream/savespersonobjecttodisk.htm \n https://cplusplus.com/forum/beginner/106630/", font, 20);
     creditsText.setFillColor(sf::Color::White);
-    creditsText.setPosition(100, 250);
+    creditsText.setPosition(60, 250);
 
     while (window.isOpen()) {
         sf::Event event;
